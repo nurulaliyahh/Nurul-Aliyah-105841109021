3fa111c97fbf89e207eb0d23b44f2ba5f0378c2a
nurulaliyahh <137029409+nurulaliyahh@users.noreply.github.com>
2024-05-29 15:05:39 +0800

    Aliyah2

--- App.js
+++ App.js
@@ -1,26 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
 import React from 'react'
+const App = () => {
+  return(
+    <View style={{ flex: 1 }}>
+    <View style={{
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'flex-end', // baris
+      justifyContent: 'center', // Kolom
+      marginBottom: 100,
 
-const App = () =>{
-  return (
-    <View style ={{ flex: 1 }}>
-      <View style={{ flex: 1, flexDirection:"row", backgroundColor:'white'}}>
-      <View style={{ flex: 1, backgroundColor:'yellow'}}>
-    </View>
-    <View style={{ flex: 1, backgroundColor:'green'}}>
-    </View>
-
-    </View>
-      <View style={{ flex: 1, flexDirection:'row', backgroundColor:'red'}}>
-      <View style={{ flex: 1, backgroundColor:'pink'}}>
-    </View>
-    
-    <View style={{ flex: 1, backgroundColor:'brown'}}>
-    </View>
-    </View>
-    <View style={{ flex: 1, backgroundColor:'blue'}}>
-    </View>
-    </View>
+    }}>
+      <View style={{
+        flex:1,
+        backgroundColor: 'red', width: 130, height: 60,
+        justifyContent: 'center',
+        borderRadius: 10,
+        marginRight:10,
+        marginLeft: 10,
+      }}>
+        <Text style={{
+          fontSize: 20 , color: 'white',
+          textAlign: 'center',
+          fontWeight: 'bold'
+        }}>
+          Login
+        </Text>
+        </View>
+        <View style={{
+        flex:1,
+        backgroundColor: 'red', width: 130, height: 60,
+        justifyContent: 'center',
+        borderRadius: 10,
+        marginRight: 10,
+      }}>  
+        <Text style={{
+          fontSize: 20 , color: 'white',
+          textAlign: 'center',
+          fontWeight: 'bold'
+        }}>
+          Register
+        </Text>
+        </View>
+      </View>
+      </View>
   )
 }
 export default App
