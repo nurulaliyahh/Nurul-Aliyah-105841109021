31ceedfae93e168654ec9b629ab12512d1fa7176
nurulaliyahh <137029409+nurulaliyahh@users.noreply.github.com>
2024-06-12 15:57:34 +0800

    componentbutton_105841109021

--- App.js
+++ App.js
@@ -1,28 +1,8 @@
 import { StyleSheet, Text, View, Image} from 'react-native';
 import React from 'react'
+import ButtonCustom from './component/Button/Button'
 const App = () => {
-  const ButtonCustom = ({color, text}) => {
 
-    return(
-      <View style={{
-        flex:1,
-        backgroundColor: color,
-         width: 130, 
-         height: 60,
-        justifyContent: 'center',
-        borderRadius: 10,
-        marginRight: 10,
-      }}>  
-        <Text style={{
-          fontSize: 20 , color: 'white',
-          textAlign: 'center',
-          fontWeight: 'bold'
-        }}>
-          {text}
-        </Text>
-        </View>
-    )
-  }
   return(  
     <View style={{ flex: 1 }}>
       <View style={{
